03e03fa69033ee8c5c7bf61fec45a663abbfad28
Barros9 <giu.barreca@gmail.com>
2022-12-01 10:39:10 +0100

    Day01 - Solution

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -5,17 +5,40 @@ import readInput
 
 class Day01 : Day {
     private fun part1(input: List<String>): Int {
-        return 0
+        return input
+            .asSequence()
+            .flatMapIndexed { index, value ->
+                when {
+                    index == 0 || index == input.lastIndex -> listOf(index)
+                    value.isEmpty() -> listOf(index - 1, index + 1)
+                    else -> emptyList()
+                }
+            }
+            .windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
+            .maxOfOrNull { elf -> elf.sumOf { it.toInt() } } ?: 0
     }
 
     private fun part2(input: List<String>): Int {
-        return 0
+        return input
+            .asSequence()
+            .flatMapIndexed { index, value ->
+                when {
+                    index == 0 || index == input.lastIndex -> listOf(index)
+                    value.isEmpty() -> listOf(index - 1, index + 1)
+                    else -> emptyList()
+                }
+            }
+            .windowed(size = 2, step = 2) { (from, to) -> input.slice(from..to) }
+            .map { elf -> elf.sumOf { it.toInt() } }
+            .sortedDescending()
+            .take(3)
+            .sum()
     }
 
     override fun play() {
         val testInput = readInput("day01/Day01_test")
-        check(part1(testInput) == 0)
-        check(part2(testInput) == 0)
+        check(part1(testInput) == 24000)
+        check(part2(testInput) == 45000)
 
         val input = readInput("day01/Day01")
         println("Day 01")
